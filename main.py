e7db3b34e78c11216032a6ac95d34cf04a802e6d
edmond mulera <edmondmulera55@gmail.com>
2024-03-25 08:53:13 +0300

    data.csv file

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import pandas
 
-df = pandas
+df = pandas.read_csv("hotels.csv")
+
 
 class Hotel:
+    def __init__(self, id):
+        pass
     def book(self):
+        pass
+    def available(self):
         pass
 
 
 class Reservation:
     def generate(self):
         pass
+
+
+print(df)
+id = input("Enter the id of the hotel")
+hotel = Hotel(id):
+if hotel.availabel():
+    hotel.book()
