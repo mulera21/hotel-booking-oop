ce8d327dd4544e631e624b702428d4d1d29557c6
edmond mulera <edmondmulera55@gmail.com>
2024-03-25 09:05:15 +0300

    main logic of the hotel

--- main.py
+++ main.py
@@ -6,19 +6,31 @@ df = pandas.read_csv("hotels.csv")
 class Hotel:
     def __init__(self, id):
         pass
+
     def book(self):
         pass
+
     def available(self):
         pass
 
 
 class Reservation:
+    def __init__(self, customer_name, hotel_object):
+        pass
+
     def generate(self):
         pass
 
 
 print(df)
 id = input("Enter the id of the hotel")
-hotel = Hotel(id):
+hotel = Hotel(id)
+
+
 if hotel.availabel():
     hotel.book()
+    name = input("Enter your name")
+    reservation = Reservation(name, hotel)
+    print(reservation.generate())
+else:
+    print("Hotel is not free")
